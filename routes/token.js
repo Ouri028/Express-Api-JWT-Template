de0bfa8843d63d90fc001b9649977dda20cef1c9
Sylvester Stephenson <49057553+Ouri028@users.noreply.github.com>
2021-12-13 18:12:28 +0200

    Update token.js
    fixed logical issue with token validation

--- routes/token.js
+++ routes/token.js
@@ -2,50 +2,49 @@ const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const TOKEN = Router();
+const config = process.env;
 
 
 TOKEN.post("/token", async (req, res) => {
+    // Get user input
+    const { user, password } = req.body;
+    // Validate user input
+    if (user && password) {
+      if (user == config.TOKEN_USER && password == config.TOKEN_SECRET) {
+        const token_user = process.env.TOKEN_USER;
 
-    // Our register logic starts here
-    try {
-      // Get user input
-      const { user, password } = req.body;
-      // Validate user input
-      if (!(user && password)) {
+        //Encrypt user password
+        const encryptedPassword = await bcrypt.hash(password, 10).catch(err => console.error(err));
+
+        // Create token
+        const token = jwt.sign(
+          { user: user._id },
+          process.env.TOKEN_SECRET,
+          {
+            expiresIn: "2h",
+          }
+        );
+
+        let tokenized = {
+          user: user,
+          token: token
+        }
+        // return new user
+        return res.send(tokenized).status(200);
+      } else {
         return res.status(400).send({
-            error: "AUTH_FAILED",
-            message: "Required parameters { user: <String>, password: <String> }"
+          error: "AUTH_FAILED",
+          message: "Required parameters { user: <String>, password: <String> }"
         });
       }
-
-      const token_user = process.env.TOKEN_USER;
-  
-      //Encrypt user password
-      const encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create token
-      const token = jwt.sign(
-        { user: user._id },
-        process.env.TOKEN_SECRET,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      let tokenized = {
-        user: user,
-        token: token
+    }  else {
+      return res.status(400).send({
+        error: "AUTH_FAILED",
+        message: "Required parameters { user: <String>, password: <String> }"
+      });
     }
-      // return new user
-      return res.send(tokenized).status(200);
-    } catch (err) {
-      return res.send({
-          error: err.message,
-      }).status(400);
-    }
-    // Our register logic ends here
-  });
+});
 
 module.exports = {
-    TOKEN
+  TOKEN
 }
